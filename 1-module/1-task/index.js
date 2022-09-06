5e53659688ea057418512a7584f46c55dca80c41
Ваше Имя <catsmadz@gmail.com>
2022-09-06 22:59:37 +0500

    Вычисление факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  let result = n
+  if(n == 0 || n == 1){
+    result = 1
+  }
+  else{
+    result = 1;
+    while(n){
+      result *= n--
+    }
 }
+
+  return result
+}
+console.log(factorial(5))
