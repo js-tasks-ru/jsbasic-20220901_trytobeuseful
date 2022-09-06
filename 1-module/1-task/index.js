ee9c2d13b1f8ef654c8cf8290f017e42742aecfc
Ваше Имя <catsmadz@gmail.com>
2022-09-06 23:11:18 +0500

    Вычислить факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,15 +1,7 @@
 function factorial(n) {
-  let result = n
-  if(n == 0 || n == 1){
-    result = 1
-  }
-  else{
     result = 1;
     while(n){
       result *= n--
     }
-}
-
   return result
 }
-console.log(factorial(5))
