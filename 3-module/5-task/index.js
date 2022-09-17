734d980f8ead2b610f061fc7d161a53812f108aa
Ваше Имя <catsmadz@gmail.com>
2022-09-17 16:21:50 +0500

    5 задача

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = [];
+  str.split(' ').map(element => {
+     if(!isNaN(Number(element))){
+       numbers.push(Number(element))
+     }
+     
+  });
+  return {
+    min:(Math.min.apply(null, numbers)),
+    max:(Math.max.apply(null, numbers)),
+  }
 }
